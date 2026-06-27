869cb5a44cb59080572783f745f1a0210b63b7f8
Vishal Ambekar <vishalambekar123@gmail.com>
2026-06-27 21:24:16 +0530

    changed to safari

--- playwright.config.js
+++ playwright.config.js
@@ -36,8 +36,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
     },
 
     /*{
